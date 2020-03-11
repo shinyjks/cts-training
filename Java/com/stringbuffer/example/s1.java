5faba18da5726a8a3c6c05a94b31e2008d74f2a1
Shiny <32192009+shinyjks@users.noreply.github.com>
2020-03-11 16:40:59 +0530

    Add files via upload

---
+++ Java/com/stringbuffer/example/s1.java
@@ -0,0 +1,17 @@
+package com.stringbuffer.example;
+
+public class s1 {
+
+	public static void main(String[] args) {
+		//Construct empty StringBuffer with initial capacity
+		StringBuffer s1 = new StringBuffer();
+		StringBuffer s2 = new StringBuffer("SRM IST");
+		StringBuffer s3 = new StringBuffer("Hello World");
+		System.out.println(s3);
+		System.out.println(s2);
+		System.out.println(s1);
+		
+
+	}
+
+}
---
+++ Java/com/stringbuffer/example/s2.java
@@ -0,0 +1,12 @@
+package com.stringbuffer.example;
+
+public class s2 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Hello World");
+		int len = sb.length();
+		System.out.println("Length of a StringBuffer object is : "+len);
+
+	}
+
+}
