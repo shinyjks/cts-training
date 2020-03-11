b4914140f44119aa587071098a35b1998e29d003
Shiny <32192009+shinyjks@users.noreply.github.com>
2020-03-11 16:23:25 +0530

    Add files via upload

---
+++ Java/com/stringbuffer/example/app.java
@@ -0,0 +1,13 @@
+package com.stringbuffer.example;
+
+public class app {
+
+	public static void main(String[] args) {
+		boolean b = false;
+		StringBuffer sb1 = new StringBuffer("Boolean Append     ");
+		sb1.append(b);
+		System.out.println(sb1);
+
+	}
+
+}
---
+++ Java/com/stringprogram/example/TrimExampleCode.java
@@ -0,0 +1,9 @@
+package com.stringprogram.example;
+
+public class TrimExampleCode {
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Java");
+	}
+
+}
