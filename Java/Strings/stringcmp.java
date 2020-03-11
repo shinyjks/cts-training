6a437c5063cb634a7ee0f4b60ffcf8b7d37a874e
Shiny <32192009+shinyjks@users.noreply.github.com>
2020-03-11 13:49:17 +0530

    Add files via upload

---
+++ Java/Strings/stringcmp.java
@@ -0,0 +1,19 @@
+public class stringcmp
+{
+	public static void main(String[] args) 
+	{
+		//Creating string objects using literals
+
+		String s1 = "abc";
+		String s2 = "abc";
+
+		System.out.println(s1 == s2);	//Output = true
+
+		//Creating String objects using new operator
+
+		String s3 = new String("abc");
+		String s4 = new String("abc");
+
+		System.out.println(s3 == s4);	//Output = false
+	}
+}
