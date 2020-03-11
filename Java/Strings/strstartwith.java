9f54b47df7c48acf268a728dca2fef88c001a16f
Shiny <32192009+shinyjks@users.noreply.github.com>
2020-03-11 15:03:54 +0530

    Add files via upload

---
+++ Java/Strings/strstartwith.java
@@ -0,0 +1,16 @@
+public class strstartwith
+{
+	public static void main(String[] args) 
+	{
+		String str1 = "Shiny JK welcome to java";
+
+		if(str1.startsWith("Shiny"))
+		{
+			System.out.println("String starts with Shiny");
+		}
+		else
+		{
+			System.out.println("String does not start with Shiny");
+		}
+	}
+}
