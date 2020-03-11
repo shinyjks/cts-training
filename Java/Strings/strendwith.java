4111bd573d4708b43076c51efc50f34e95486df0
Shiny <32192009+shinyjks@users.noreply.github.com>
2020-03-11 14:52:45 +0530

    Add files via upload

---
+++ Java/Strings/strendwith.java
@@ -0,0 +1,16 @@
+public class strendwith
+{
+	public static void main(String[] args) 
+	{
+		String str1 = "Shiny JK welcome to java";
+
+		if(str1.endsWith("JK"))
+		{
+			System.out.println("String ends with JK");
+		}
+		else
+		{
+			System.out.println("String does not end with JK");
+		}
+	}
+}
